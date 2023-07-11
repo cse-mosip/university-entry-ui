c106e97ea6b7865e52045b76c77a7a5640e5628e
Pahan99 <isurupahan321@gmail.com>
2023-07-12 04:05:16 +0530

    Add home screen ui

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,55 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/uom.jpg)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          padding: '10px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'start',
+          height: '100%',
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            fontWeight: 'bold',
+            color: '#012970',
+          }}
+        >
+          University Entrance Identity System
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{
+            color: 'white',
+            backgroundColor: '#012970',
+            marginBottom: '30px',
+            textTransform: 'none'
+          }}
+        >
+          Device Setup
+        </Button>
+      </Box>
+    </Box>
   )
 }
 
