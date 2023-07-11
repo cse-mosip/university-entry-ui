7ff2c5a9b9ba8bfc9cd29cb4e33fafce3c792b24
Dinuka <dinukaw.19@cse.mrt.ac.lk>
2023-07-12 04:01:25 +0530

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { StyledRoot, StyledBox, StyledInputField, StyledLabel, StyledTextField, StyledButtonBox } from './LoginStyles';
 
-function Login() {
+
+const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <StyledRoot>
+      <StyledBox>
+        <Typography variant="h5" sx={{ color: '#198754', fontWeight: 700, marginBottom: '30px' }}>
+          Login to Your Account
+        </Typography>
+        <StyledInputField>
+          <StyledLabel variant="body1">Username</StyledLabel>
+          <StyledTextField variant="outlined" />
+        </StyledInputField>
+        <StyledInputField>
+          <StyledLabel variant="body1">Password</StyledLabel>
+          <StyledTextField variant="outlined" type="password" />
+        </StyledInputField>
+        <StyledButtonBox>
+          <Button variant="contained" sx={{ bgcolor: '#4154F1', '&:hover': { backgroundColor: '#3249C9' }, padding: '8px 25px', fontSize: '16px' }}>
+            Login
+          </Button>
+          <Button variant="contained" sx={{ bgcolor: '#DC3545', '&:hover': { backgroundColor: '#C02942' }, padding: '8px 25px', fontSize: '16px' }}>
+            Cancel
+          </Button>
+        </StyledButtonBox>
+      </StyledBox>
+    </StyledRoot>
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/pages/LoginStyles.js
@@ -0,0 +1,66 @@
+import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+export const StyledRoot = styled('div')({
+    height: '100vh',
+    backgroundImage: 'url(/images/bg.jpg)',
+    backgroundSize: 'cover',
+    backgroundOpacity: 0.5,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  });
+  
+export const StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: '#CDCDCD',
+    borderRadius: 10,
+    padding: theme.spacing(8),  // Padding inside the box
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    width: 700,
+    height: 300,
+    gap: theme.spacing(2),  // Space between elements in the box
+  }));
+  
+export const StyledInputField = styled(Box)(({ theme }) => ({
+    marginBottom: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: '10px',
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+      alignItems: 'flex-start'
+    },
+  }));
+  
+export const StyledLabel = styled(Typography)(({ theme }) => ({
+    width: 100,
+    marginRight: theme.spacing(2),
+    fontSize: 20,
+  }));
+  
+export const StyledTextField = styled(TextField)(({ theme }) => ({
+    '& .MuiInputBase-input': {
+      color: 'black',
+    },
+    '& .MuiInputBase-root': {
+      backgroundColor: 'white',
+    },
+    flex: 1,
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+  }));
+  
+export const StyledButtonBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'flex-end',
+    gap: '10px',
+    marginTop: 'auto',
+    width: '100%',
+  });
