16a9a8ad926c938e0b8cb4ba509f904c0fd68ef2
Madara Semini <pgmsgunarathna@gmail.com>
2023-07-12 10:46:56 +0530

    create studentDetails modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RouteAuthMiddleware from './middlewares/RouteAuthMiddleware';
 import GuestRegistration from './pages/GuestRegistration';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import StudentCard from './pages/studentDetails';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Route element={<Login/>} path='/login'/>
 				<Route element={<RouteAuthMiddleware role={'role'}><GuestRegistration/></RouteAuthMiddleware>} path='guest-registration'/>
 				<Route element={<Home/>} path='/'/>
+        <Route element={<StudentCard/>} path='/stud'/>
 			</Routes>
 		</BrowserRouter>
 	);
