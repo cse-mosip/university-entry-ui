414eaa4f3ce67218d2328e9c9b245c463da35174
YumeChaan <chan.19@cse.mrt.ac.lk>
2023-07-12 09:42:42 +0530

    Guest registration ui implemented

--- src/pages/GuestRegistration.jsx
+++ src/pages/GuestRegistration.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+import {
+  StyledRoot,
+  StyledBox,
+  StyledLabel,
+  StyledTextField,
+  StyledSelect,
+  StyledRadioGroup,
+  StyledRadio,
+  StyledButtonBox
+} from "./GusetRegistrationStyles";
 
 function GuestRegistration() {
+  const [title, setTitle] = React.useState("Mr.");
+  const [gender, setGender] = React.useState("male");
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
   return (
-    <div>GuestRegistration</div>
-  )
+    <StyledRoot>
+      <Typography
+          variant="h5"
+          sx={{ color: "#4154F1", fontWeight: 700, marginBottom: "30px",paddingTop: "30px", paddingLeft: "10%" }}
+        >
+          Guest Registration Form
+        </Typography>
+      <StyledBox>
+        <Typography
+          variant="h5"
+          sx={{ color: "#198754", fontWeight: 700, marginBottom: "30px" }}
+        >
+          Guest Details
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <StyledLabel>Name</StyledLabel>
+          </Grid>
+
+          <Grid item xs={2}>
+            <StyledSelect variant="outlined" value={title}>
+              <MenuItem value="Mr.">Mr.</MenuItem>
+              <MenuItem value="Mrs.">Mrs.</MenuItem>
+              <MenuItem value="Miss.">Miss.</MenuItem>
+            </StyledSelect>
+          </Grid>
+
+          <Grid item xs={6}>
+            <StyledTextField variant="outlined" />
+          </Grid>
+
+          <Grid item xs={4}>
+            <StyledLabel>Phone Number</StyledLabel>
+          </Grid>
+
+          <Grid item xs={8}>
+            <StyledTextField variant="outlined" />
+          </Grid>
+
+          <Grid item xs={4}>
+            <StyledLabel>NIC</StyledLabel>
+          </Grid>
+
+          <Grid item xs={8}>
+            <StyledTextField variant="outlined" />
+          </Grid>
+
+          <Grid item xs={4}>
+            <StyledLabel>Gender</StyledLabel>
+          </Grid>
+
+          <Grid item xs={8}>
+            <StyledRadioGroup
+              aria-label="gender"
+              name="gender"
+              value={gender}
+              onChange={handleChange}
+            >
+              <FormControlLabel
+                value="male"
+                control={<StyledRadio />}
+                label="Male"
+              />
+              <FormControlLabel
+                value="female"
+                control={<StyledRadio />}
+                label="Female"
+              />
+            </StyledRadioGroup>
+          </Grid>
+        </Grid>
+        <Typography
+          variant="h5"
+          sx={{ color: "#198754", fontWeight: 700, marginBottom: "30px", marginTop: "30px" }}
+        >
+          Inviter
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <StyledLabel>Name</StyledLabel>
+          </Grid>
+
+          <Grid item xs={2}>
+            <StyledSelect variant="outlined" value={title}>
+              <MenuItem value="Mr.">Mr.</MenuItem>
+              <MenuItem value="Mrs.">Mrs.</MenuItem>
+              <MenuItem value="Miss.">Miss.</MenuItem>
+            </StyledSelect>
+          </Grid>
+
+          <Grid item xs={6}>
+            <StyledTextField variant="outlined" />
+          </Grid>
+
+          <Grid item xs={4}>
+            <StyledLabel>Index</StyledLabel>
+          </Grid>
+          <Grid item xs={8}>
+            <StyledTextField variant="outlined" />
+          </Grid>
+        </Grid>
+
+        <StyledButtonBox>
+        <Button variant="contained" sx={{ bgcolor: '#4154F1', '&:hover': { backgroundColor: '#3249C9' }, padding: '8px 25px', fontSize: '16px' }}>
+            Proceed
+          </Button>
+          <Button variant="contained" sx={{ bgcolor: '#DC3545', '&:hover': { backgroundColor: '#C02942' }, padding: '8px 25px', fontSize: '16px' }}>
+            Cancel
+          </Button>
+        </StyledButtonBox>
+
+      </StyledBox>
+    </StyledRoot>
+  );
 }
 
-export default GuestRegistration
+export default GuestRegistration;
---
+++ src/pages/GusetRegistrationStyles.js
@@ -0,0 +1,76 @@
+import { styled } from "@mui/system";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Select from "@mui/material/Select";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import TextField from "@mui/material/TextField";
+
+export const StyledRoot = styled("div")({
+  height: "100vh",
+//   display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#F6F9FF",
+});
+
+export const StyledBox = styled(Box)(({ theme }) => ({
+    margin: "auto",
+  width: "60%",
+  border: "1px solid #D9D9D9",
+  padding: theme.spacing(4),
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: theme.spacing(2),
+}));
+
+export const StyledLabel = styled(Typography)(({ theme }) => ({
+  width: "100%",
+  fontSize: 20,
+  paddingTop: theme.spacing(1),
+}));
+
+export const StyledTextField = styled(TextField)(({ theme }) => ({
+  "& .MuiInputBase-input": {
+    color: "black",
+  },
+  "& .MuiInputBase-root": {
+    backgroundColor: "white",
+  },
+  width: "100%",
+}));
+
+export const StyledSelect = styled(Select)(({ theme }) => ({
+  width: "100%",
+  "& .MuiSelect-select": {
+    color: "black",
+    backgroundColor: "white",
+    paddingLeft: "10px",
+  },
+}));
+
+export const StyledRadioGroup = styled(RadioGroup)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  "& .MuiFormControlLabel-label": {
+    color: "black",
+  },
+  "& .Mui-checked": {
+    color: "black",
+  },
+}));
+
+export const StyledRadio = styled(Radio)(({ theme }) => ({
+  "&.Mui-checked": {
+    color: "black",
+  },
+}));
+
+export const StyledButtonBox = styled(Box)({
+  display: "flex",
+  justifyContent: "flex-end",
+  gap: "10px",
+  marginTop: "30px",
+  width: "100%",
+});
