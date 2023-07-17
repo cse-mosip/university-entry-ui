26925650c92540d1748a4c575f22261451f597c8
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-07-17 11:17:38 +0530

    added toastify

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RouteAuthMiddleware from './middlewares/RouteAuthMiddleware';
-import GuestRegistration from './pages/GuestRegistration';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import StudentCard from './pages/studentDetails';
-import AdminView from './pages/AdminView';
-import DeviceSetup from './pages/DeviceSetup';
-import TopBar from './components/topBar';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RouteAuthMiddleware from "./middlewares/RouteAuthMiddleware";
+import GuestRegistration from "./pages/GuestRegistration";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import StudentCard from "./pages/studentDetails";
+import AdminView from "./pages/AdminView";
+import DeviceSetup from "./pages/DeviceSetup";
+import TopBar from "./components/topBar";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
-	return (
-		<>
-			<TopBar />
-			<BrowserRouter>
-				<Routes>
-					<Route element={<DeviceSetup />} path='/device-setup' />
-					<Route element={<RouteAuthMiddleware role={'role'}><GuestRegistration /></RouteAuthMiddleware>} path='guest-registration' />
-					<Route element={<RouteAuthMiddleware role={'role'}><AdminView /></RouteAuthMiddleware>} path='entry-management' />
-					<Route element={<StudentCard isOpen={true} />} path='/stu' />
-					<Route element={<Home />} path='/home' />
-					<Route element={<Login />} path='/' />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <TopBar />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DeviceSetup />} path="/device-setup" />
+          <Route
+            element={
+              <RouteAuthMiddleware role={"role"}>
+                <GuestRegistration />
+              </RouteAuthMiddleware>
+            }
+            path="guest-registration"
+          />
+          <Route
+            element={
+              <RouteAuthMiddleware role={"role"}>
+                <AdminView />
+              </RouteAuthMiddleware>
+            }
+            path="entry-management"
+          />
+          <Route element={<StudentCard isOpen={true} />} path="/stu" />
+          <Route element={<Home />} path="/home" />
+          <Route element={<Login />} path="/" />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const http = axios.create({
-    // baseURL: 'http://localhost:80',
-    baseURL: 'http://localhost:8080/api/v1',
-    withCredentials: true,
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // }
+  // baseURL: 'http://localhost:80',
+  baseURL: "http://104.211.229.43/entry-service/api/v1",
+  // baseURL: 'http://localhost:8080/api/v1',
+  withCredentials: true,
+  // headers: {
+  //     'Content-Type': 'application/json'
+  // }
 });
 
 export default http;
