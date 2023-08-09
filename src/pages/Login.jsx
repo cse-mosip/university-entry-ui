873d33b16f4d28e86d3e7cde10bc53af2610aad1
Dinuka <dinukaw.19@cse.mrt.ac.lk>
2023-08-09 11:35:28 +0530

    form validation: login UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import Typography from '@mui/material/Typography';
-import { StyledRoot, StyledBox, StyledInputField, StyledLabel, StyledTextField, StyledButtonBox, StyledButton, StyledTypography } from './LoginStyles';
+import React from 'react';
+import {useFormik} from "formik"
+import * as Yup from 'yup';
+import { 
+  StyledRoot, 
+  StyledBox, 
+  StyledInputField, 
+  StyledLabel, 
+  StyledTextField, 
+  StyledButtonBox, 
+  StyledButton, 
+  StyledTypography 
+} from './LoginStyles';
+
+const validationSchema = Yup.object({
+  username: Yup.string().required('Username is required').matches(/^[A-Za-z\s]+$/, 'Invalid username'),
+  password: Yup.string().required('Password is required').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, 'Invalid password (Minimum eight characters, at least one letter and one number)'),
+});
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoginDisabled, setLoginDisabled] = useState(true);
 
-  useEffect(() => {
-    if (username.trim() && password.trim()) {
-      setLoginDisabled(false);
-    } else {
-      setLoginDisabled(true);
+  const loginFormik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values, {resetForm}) => {
+      console.log(values);
+      // call api
+      resetForm();
     }
-  }, [username, password]);
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  }
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  }
-
-  const handleLogin = () => {
-    // api call
-    console.log({ username, password });
-  }
+  });
 
   return (
     <StyledRoot>
@@ -40,8 +44,14 @@ const Login = () => {
           </StyledLabel>
           <StyledTextField 
             variant="outlined" 
-            value={username}
-            onChange={handleUsernameChange}
+            name = "username" 
+            id="username" 
+            type="text" 
+            value={loginFormik.username} 
+            onChange={loginFormik.handleChange} 
+            onBlur={loginFormik.handleBlur} 
+            error={Boolean(loginFormik.touched.username && loginFormik.errors.username)} 
+            helperText={loginFormik.touched.username && loginFormik.errors.username}
           />
         </StyledInputField>
         <StyledInputField>
@@ -50,18 +60,21 @@ const Login = () => {
           </StyledLabel>
           <StyledTextField 
             variant="outlined" 
+            name = "password" 
+            id="password" 
             type="password" 
-            value={password}
-            onChange={handlePasswordChange}
+            value={loginFormik.password} 
+            onChange={loginFormik.handleChange} 
+            onBlur={loginFormik.handleBlur} 
+            error={Boolean(loginFormik.touched.password && loginFormik.errors.password)} 
+            helperText={loginFormik.touched.password && loginFormik.errors.password}
           />
         </StyledInputField>
         <StyledButtonBox>
           <StyledButton 
             variant="contained" 
             sx={{ bgcolor: '#012970', '&:hover': { bgcolor: '#011538' }}}
-            disabled={isLoginDisabled}
-            // onClick={handleLogin}
-            onSubmit={handleLogin}
+            onClick={loginFormik.handleSubmit}
           >
             Login
           </StyledButton>
